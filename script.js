feaf033d414435b77d689afb60862e9321009a2b
fayez <faezzzakmal@gmail.com>
2026-07-29 15:54:41 +0800

    Created Tech Store website

---
+++ script.js
@@ -0,0 +1,92 @@
+let cart = 0;
+
+
+
+function addCart() {
+
+    cart = cart + 1;
+
+
+    document.getElementById("cart").innerHTML = cart;
+
+
+}
+
+
+
+
+function toggleDarkMode() {
+
+
+    document.body.classList.toggle("dark");
+
+
+}
+
+
+
+
+
+
+function searchProduct() {
+
+
+    let input = document
+        .getElementById("search")
+        .value
+        .toLowerCase();
+
+
+
+    let products = document
+        .getElementsByClassName("card");
+
+
+
+
+    for (let i = 0; i < products.length; i++) {
+
+
+        let name = products[i]
+            .innerText
+            .toLowerCase();
+
+
+
+        if (name.includes(input)) {
+
+
+            products[i].style.display = "block";
+
+
+        }
+
+
+        else {
+
+
+            products[i].style.display = "none";
+
+
+        }
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+function showMessage() {
+
+
+    alert("Thanks for contacting Tech Store!");
+
+
+}
